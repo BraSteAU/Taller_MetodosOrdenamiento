c8073847611009d376a6c4e49a3b21ec480eca6f
BraSteAU <brayan_8924@hotmail.com>
2025-01-28 21:19:17 -0500

    Ejercicio 5
    Ejercicio 5 resuelto

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,9 +1,7 @@
 #Ordenación de precios de productos: Un sistema de inventario tiene precios desordenados.
 #Ordénalos ascendentemente con burbuja.
 
-precios=input("Ingresa los precios de los productos: ").split()
-for i in range(len(precios)):
-    precios[i]=float(precios[i])
+precios=[10450.4, 10000.45, 5700.99, 24500.57, 14999.99, 2145.36]
 
 n=len(precios)
 for i in range(n):
